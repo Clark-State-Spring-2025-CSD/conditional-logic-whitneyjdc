082bff77361baa14a0ccc031a9074fa3a7be8cdc
whitneyjdc <whitneyjdc@gmail.com>
2025-02-15 22:30:31 -0500

    Update fixing_bugs.py

--- fixing_bugs.py
+++ fixing_bugs.py
@@ -13,9 +13,9 @@
 selection = input("Enter a lowercase letter")
 
 if selection == "a":
-print("You entered a")
-elif selection = "b":
-print("You entered a b")
-else
-    print("Well it wasn't an a or b.")
+    print("You entered a")
+elif selection == "b":
+    print("You entered a b")
+else:
+    print("Well it wasn't an a or b")
 
